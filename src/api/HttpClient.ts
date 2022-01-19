5494b4aab64fbbf87e841a212cdc378b3f84a686
RobinNusantara <robinnusantara127@gmail.com>
2022-01-19 21:17:57 +0700

    [ADD] http client api superclass

---
+++ src/api/HttpClient.ts
@@ -0,0 +1,26 @@
+/* eslint-disable @typescript-eslint/explicit-module-boundary-types */
+import axios, { AxiosInstance, AxiosResponse } from "axios";
+
+export abstract class HttpClient {
+    protected readonly _axiosInstance: AxiosInstance;
+
+    constructor(baseUrl: string, headers?: object) {
+        this._axiosInstance = axios.create({
+            baseURL: baseUrl,
+            headers: { ...headers },
+        });
+
+        this.initializeResponseInterceptor();
+    }
+
+    private initializeResponseInterceptor = () => {
+        this._axiosInstance.interceptors.response.use(
+            this.handleResponse,
+            this.handleError,
+        );
+    };
+
+    private handleResponse = (response: AxiosResponse) => response;
+
+    protected handleError = (error: any): Promise<any> => Promise.reject(error);
+}
